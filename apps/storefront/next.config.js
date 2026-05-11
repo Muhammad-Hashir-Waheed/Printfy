281035fd4ce58740da9ddfe3e7ef6c53cdd3f55e
Muhammad Hashir Waheed <hashirwaheed07@gmail.com>
2026-05-11 20:43:22 +0500

    Add domain packages and update Next.js configuration

--- apps/storefront/next.config.js
+++ apps/storefront/next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 
+const path = require('path')
+
 module.exports = {
+    transpilePackages: [
+        'domain-checkout',
+        'domain-fulfillment',
+        'domain-orders',
+        'domain-pricing',
+    ],
+    experimental: {
+        outputFileTracingRoot: path.join(__dirname, '../../'),
+    },
+    webpack: (config) => {
+        config.resolve.modules = [
+            path.resolve(__dirname, 'node_modules'),
+            ...(config.resolve.modules || []),
+        ]
+        return config
+    },
     images: {
         remotePatterns: [
             {
